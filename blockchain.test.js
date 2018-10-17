7b551ce07e3d71bdc3cd88668a9b8904ad1a7ff7
Will Nguyen <willnguyen@Wills-MacBook-Pro.local>
2018-10-17 20:41:54 +0700

    test for blockchain

--- blockchain.test.js
+++ blockchain.test.js
@@ -23,4 +23,27 @@ describe('Blockchain', () => {
         bc2.addBlock('foo')
         expect(bc.isValidChain(bc2.chain)).toBe(true);
     })
+
+    it('invalidates a chain with a corrup genesis block', () => {
+        bc2.chain[0].data = 'Bad data';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    })
+
+    it('invalidates a corrupt chain', () => {
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not foo';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    })
+
+    it('replaces the chian with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).toEqual(bc2.chain);
+    })
+
+    it('does not replace the chain with one of less than or equal to length', () => {
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain);
+        expect(bc.chain).not.toEqual(bc2.chain);
+    })
 });
